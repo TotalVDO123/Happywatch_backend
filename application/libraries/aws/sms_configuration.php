4059bd0b6c10538218dc39665579670b7f0a5015
ratneshk500 <168002226+ratneshk500@users.noreply.github.com>
2025-04-13 18:39:30 +0530

    Update sms_configuration.php

--- application/libraries/aws/sms_configuration.php
+++ application/libraries/aws/sms_configuration.php
@@ -1,23 +1,5 @@
 <?php
-/*
-require 'vendor/autoload.php';
 
-
-
-
-use Aws\Sns\SnsClient;
-use Aws\Exception\AwsException;
-
-$s3Client = new SnsClient([
-    'region' => 'us-east-1',
-    'version' => 'latest',
-    'credentials' => [
-        'key'    => 'AKIAXRJJLDYGLQGF7JU3',
-        'secret' => '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',
-    ],
-]);
-
-*/
 
 
 //require 'vendor/autoload.php';
@@ -27,7 +9,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 $s3Client = new Aws\Sns\SnsClient([
     'region'  => 'us-east-1',
     'version' => 'latest',
-    'credentials' => ['key' => 'AKIAXRJJLDYGLQGF7JU3', 'secret' => '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']
+    'credentials' => ['key' => '', 'secret' => '']
   ]);
 
 
